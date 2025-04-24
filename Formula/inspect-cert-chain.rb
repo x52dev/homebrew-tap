feb299c5e45da63ed3bdc8f4114af65a52a49a96
Rob Ede <robjtede@icloud.com>
2025-04-24 21:25:06 +0100

    update inspect-cert-chain to 0.0.21

--- Formula/inspect-cert-chain.rb
+++ Formula/inspect-cert-chain.rb
@@ -1,18 +1,18 @@
 class InspectCertChain < Formula
   desc "Inspect and debug TLS certificate chains (without OpenSSL)"
   homepage "https://github.com/robjtede/inspect-cert-chain"
-  version "0.0.20"
+  version "0.0.21"
   license "MIT OR Apache-2.0"
 
   on_macos do
     on_arm do
       url "https://github.com/robjtede/inspect-cert-chain/releases/download/v#{version}/inspect-cert-chain-aarch64-apple-darwin.tar.gz"
-      sha256 "c603a9082e670e80e9bc37f414b570634401f1768b7baeb9009faa779f954d5c"
+      sha256 "01bd0b473e5e2514eb34faeb9759e8edeaabbfe9ac7ec7a52ccf2aaeae9583e3"
     end
 
     on_intel do
       url "https://github.com/robjtede/inspect-cert-chain/releases/download/v#{version}/inspect-cert-chain-x86_64-apple-darwin.tar.gz"
-      sha256 "bbcb12669acf6c0fd6d5eaef5eafec023c4e267413fa7e94ad3142a17ec04401"
+      sha256 "008ca9d8db789d64c953e8edf9e0e808f56196d5c1cb0d19acab4c67f60e00f7"
     end
 
     def install
